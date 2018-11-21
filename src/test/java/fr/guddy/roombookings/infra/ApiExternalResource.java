55c8b76cd471954c80950fa41b3dfbffeb14f95a
Romain Rochegude <romain.rochegude@gmail.com>
2018-11-21 21:00:45 +0100

    defined api external resource to test api in UT

--- src/test/java/fr/guddy/roombookings/infra/ApiExternalResource.java
+++ src/test/java/fr/guddy/roombookings/infra/ApiExternalResource.java
@@ -1,18 +1,53 @@
 package fr.guddy.roombookings.infra;
 
+import fr.guddy.roombookings.domain.bookings.Bookings;
+import fr.guddy.roombookings.domain.bookings.NitriteBookings;
+import fr.guddy.roombookings.domain.rooms.NitriteRooms;
 import fr.guddy.roombookings.domain.rooms.Rooms;
+import org.dizitart.no2.Nitrite;
 import org.junit.rules.ExternalResource;
 
 public final class ApiExternalResource extends ExternalResource {
 
     private final Api api;
+    private final Rooms rooms;
+    private final Bookings bookings;
 
-    private ApiExternalResource(final Api api) {
+    private ApiExternalResource(final Api api, final Rooms rooms, final Bookings bookings) {
         this.api = api;
+        this.rooms = rooms;
+        this.bookings = bookings;
     }
 
-    public ApiExternalResource(final int port, final Rooms rooms) {
-        this(new Api(port, rooms));
+    public ApiExternalResource(final int port, final Rooms rooms, final Bookings bookings) {
+        this(
+                new Api(port, rooms, bookings),
+                rooms,
+                bookings
+        );
+    }
+
+    public ApiExternalResource(final int port, final Nitrite database) {
+        this(
+                port,
+                database,
+                new NitriteRooms(database)
+        );
+    }
+
+    public ApiExternalResource(final int port, final Nitrite database, final Rooms rooms) {
+        this(
+                port,
+                new NitriteRooms(database),
+                new NitriteBookings(database, rooms)
+        );
+    }
+
+    public ApiExternalResource() {
+        this(
+                7000,
+                Nitrite.builder().openOrCreate()
+        );
     }
 
     @Override
@@ -25,5 +60,13 @@ public final class ApiExternalResource extends ExternalResource {
     protected void after() {
         super.after();
         api.stop();
+    }
+
+    public Rooms rooms() {
+        return rooms;
+    }
+
+    public Bookings bookings() {
+        return bookings;
     }
 }
