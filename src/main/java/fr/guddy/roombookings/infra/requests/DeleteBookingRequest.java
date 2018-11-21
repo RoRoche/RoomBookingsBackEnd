bd4e660f87c91ab925cff56cd49a218d7f8f3827
Romain Rochegude <romain.rochegude@gmail.com>
2018-11-21 21:08:13 +0100

    define delete bookings request

--- src/main/java/fr/guddy/roombookings/infra/requests/DeleteBookingRequest.java
+++ src/main/java/fr/guddy/roombookings/infra/requests/DeleteBookingRequest.java
@@ -1,4 +1,46 @@
 package fr.guddy.roombookings.infra.requests;
 
-public final class DeleteBookingRequest {
+import fr.guddy.roombookings.domain.bookings.BookingNotDeletedException;
+import fr.guddy.roombookings.domain.bookings.BookingNotFoundException;
+import fr.guddy.roombookings.domain.bookings.Bookings;
+import fr.guddy.roombookings.infra.params.LongParameter;
+import fr.guddy.roombookings.infra.params.PathParameter;
+import fr.guddy.roombookings.infra.params.RequiredParameter;
+import io.javalin.Context;
+import org.dizitart.no2.Document;
+import org.dizitart.no2.WriteResult;
+import org.eclipse.jetty.http.HttpStatus;
+
+public final class DeleteBookingRequest implements Request {
+
+    private final Bookings bookings;
+    private final long id;
+
+    public DeleteBookingRequest(final Bookings bookings, final long id) {
+        this.bookings = bookings;
+        this.id = id;
+    }
+
+    public DeleteBookingRequest(final Bookings bookings, final Context context) {
+        this(
+                bookings,
+                new LongParameter(
+                        new RequiredParameter<>(
+                                new PathParameter("id", context)
+                        )
+                ).value()
+        );
+    }
+
+    @Override
+    public void perform(final Context context) {
+        final Document document = bookings.documentById(id)
+                .orElseThrow(() -> new BookingNotFoundException(id));
+        final WriteResult result = bookings.delete(document);
+        if (result.getAffectedCount() >= 1) {
+            context.status(HttpStatus.OK_200);
+        } else {
+            throw new BookingNotDeletedException(id);
+        }
+    }
 }
--- src/test/java/fr/guddy/roombookings/infra/requests/DeleteBookingRequestTest.java
+++ src/test/java/fr/guddy/roombookings/infra/requests/DeleteBookingRequestTest.java
@@ -1,5 +1,78 @@
 package fr.guddy.roombookings.infra.requests;
 
-public final class DeleteBookingRequestTest {
+import com.mashape.unirest.request.HttpRequestWithBody;
+import fr.guddy.roombookings.domain.booking.SimpleBooking;
+import fr.guddy.roombookings.domain.fixtures.*;
+import fr.guddy.roombookings.domain.room.SimpleRoom;
+import fr.guddy.roombookings.domain.slot.LogicalSlot;
+import fr.guddy.roombookings.infra.ApiExternalResource;
+import fr.guddy.roombookings.infra.assertions.WithFixtureAssertion;
+import fr.guddy.roombookings.infra.assertions.requests.RequestHasStatusCodeAssertion;
+import fr.guddy.roombookings.infra.assertions.requests.RequestWithBodyAssertion;
+import org.dizitart.no2.WriteResult;
+import org.eclipse.jetty.http.HttpStatus;
+import org.joda.time.Duration;
+import org.joda.time.Instant;
+import org.junit.ClassRule;
+import org.junit.Test;
 
+import static com.mashape.unirest.http.Unirest.delete;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class DeleteBookingRequestTest {
+    @ClassRule
+    public static final ApiExternalResource api = new ApiExternalResource();
+
+    @Test
+    public void testNotFound() {
+        new WithFixtureAssertion(
+                new ChainedFixtures(
+                        new ClearAllRoomsFixture(api.rooms()),
+                        new ClearAllBookingsFixture(api.bookings())
+                ),
+                new RequestWithBodyAssertion(
+                        new RequestHasStatusCodeAssertion(
+                                delete("http://localhost:7000/bookings/12"),
+                                HttpStatus.NOT_FOUND_404
+                        ),
+                        "No booking with id 12 were found"
+                )
+        ).check();
+    }
+
+    @Test
+    public void testOk() {
+        // given
+        new ClearAllRoomsFixture(api.rooms()).perform();
+        new ClearAllBookingsFixture(api.bookings()).perform();
+        final SimpleRoom room = new SimpleRoom("test_name", 12);
+        new CreateRoomFixture(api.rooms(), room).perform();
+        final WriteResult ids = new CreateBookingFixture(
+                api.bookings(),
+                new SimpleBooking(
+                        null,
+                        "test_user_id",
+                        room,
+                        new LogicalSlot(
+                                Instant.now().getMillis() / 1000,
+                                Instant.now().plus(Duration.standardHours(1).getMillis()).getMillis() / 1000
+                        )
+                )
+        ).perform();
+        final long id = ids.iterator().next().getIdValue();
+
+        // when
+        final HttpRequestWithBody delete = delete(
+                String.format(
+                        "http://localhost:7000/bookings/%d",
+                        id
+                )
+        );
+
+        // then
+        new RequestHasStatusCodeAssertion(delete, HttpStatus.OK_200).check();
+        assertThat(
+                api.bookings().documentById(id)
+        ).isEmpty();
+    }
 }
