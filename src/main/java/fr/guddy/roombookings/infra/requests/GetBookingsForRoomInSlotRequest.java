76bb377f8d905c7924304c87ec5af35b848afaeb
Romain Rochegude <romain.rochegude@gmail.com>
2018-11-21 21:05:51 +0100

    define get bookings for room in slot request

--- src/main/java/fr/guddy/roombookings/infra/requests/GetBookingsForRoomInSlotRequest.java
+++ src/main/java/fr/guddy/roombookings/infra/requests/GetBookingsForRoomInSlotRequest.java
@@ -1,4 +1,83 @@
 package fr.guddy.roombookings.infra.requests;
 
-public final class GetBookingsForRoomInSlotRequest {
+import fr.guddy.roombookings.domain.booking.Booking;
+import fr.guddy.roombookings.domain.booking.JsonBooking;
+import fr.guddy.roombookings.domain.bookings.Bookings;
+import fr.guddy.roombookings.domain.room.Room;
+import fr.guddy.roombookings.domain.rooms.RoomNotFoundException;
+import fr.guddy.roombookings.domain.rooms.Rooms;
+import fr.guddy.roombookings.domain.slot.LogicalSlot;
+import fr.guddy.roombookings.domain.slot.Slot;
+import fr.guddy.roombookings.infra.params.*;
+import io.javalin.Context;
+import org.eclipse.jetty.http.HttpStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class GetBookingsForRoomInSlotRequest implements Request {
+    private final Bookings bookings;
+    private final Room room;
+    private final Slot slot;
+
+    public GetBookingsForRoomInSlotRequest(final Bookings bookings, final Room room, final Slot slot) {
+        this.bookings = bookings;
+        this.room = room;
+        this.slot = slot;
+    }
+
+    public GetBookingsForRoomInSlotRequest(
+            final Rooms rooms,
+            final Bookings bookings,
+            final Context context) {
+        this(
+                rooms,
+                bookings,
+                new RequiredParameter<>(
+                        new PathParameter("name", context)
+                ),
+                new LongParameter(
+                        new RequiredParameter<>(
+                                new QueryParameter("timestamp_start", context)
+                        )
+                ),
+                new LongParameter(
+                        new RequiredParameter<>(
+                                new QueryParameter("timestamp_end", context)
+                        )
+                )
+        );
+    }
+
+    public GetBookingsForRoomInSlotRequest(
+            final Rooms rooms,
+            final Bookings bookings,
+            final Parameter<String> roomName,
+            final Parameter<Long> timestampStart,
+            final Parameter<Long> timestampEnd
+    ) {
+        this(
+                bookings,
+                rooms.namedRoom(roomName.value()).orElseThrow(() -> new RoomNotFoundException(roomName.value())),
+                new LogicalSlot(
+                        timestampStart.value(),
+                        timestampEnd.value()
+                )
+        );
+    }
+
+    @Override
+    public void perform(final Context context) {
+        final List<Booking> existingBookings = bookings.bookingsForRoomInSlot(room, slot);
+        if (existingBookings.isEmpty()) {
+            context.status(HttpStatus.NO_CONTENT_204);
+        } else {
+            context.json(
+                    existingBookings.stream()
+                            .map(JsonBooking::new)
+                            .map(JsonBooking::map)
+                            .collect(Collectors.toList())
+            ).status(HttpStatus.OK_200);
+        }
+    }
 }
--- src/test/java/fr/guddy/roombookings/infra/requests/GetBookingsForRoomInSlotRequestTest.java
+++ src/test/java/fr/guddy/roombookings/infra/requests/GetBookingsForRoomInSlotRequestTest.java
@@ -1,5 +1,122 @@
-import static org.junit.Assert.*;
+package fr.guddy.roombookings.infra.requests;
 
-public class GetBookingsForRoomInSlotRequestTest {
+import com.mashape.unirest.request.HttpRequest;
+import fr.guddy.roombookings.domain.booking.SimpleBooking;
+import fr.guddy.roombookings.domain.fixtures.*;
+import fr.guddy.roombookings.domain.room.SimpleRoom;
+import fr.guddy.roombookings.domain.slot.LogicalSlot;
+import fr.guddy.roombookings.infra.ApiExternalResource;
+import fr.guddy.roombookings.infra.assertions.WithFixtureAssertion;
+import fr.guddy.roombookings.infra.assertions.requests.RequestHasStatusCodeAssertion;
+import fr.guddy.roombookings.infra.assertions.requests.RequestWithBodyAssertion;
+import org.dizitart.no2.WriteResult;
+import org.eclipse.jetty.http.HttpStatus;
+import org.joda.time.Duration;
+import org.joda.time.Instant;
+import org.junit.ClassRule;
+import org.junit.Test;
 
+import static com.mashape.unirest.http.Unirest.get;
+
+public final class GetBookingsForRoomInSlotRequestTest {
+    @ClassRule
+    public static final ApiExternalResource api = new ApiExternalResource();
+
+    @Test
+    public void testNoContent() {
+        new WithFixtureAssertion(
+                new ChainedFixtures(
+                        new ClearAllRoomsFixture(api.rooms()),
+                        new ClearAllBookingsFixture(api.bookings()),
+                        new CreateRoomFixture(
+                                api.rooms(),
+                                new SimpleRoom("test_name", 12)
+                        )
+                ),
+                new RequestHasStatusCodeAssertion(
+                        get("http://localhost:7000/rooms/test_name/bookings")
+                                .queryString(
+                                        "timestamp_start",
+                                        Instant.now().getMillis() / 1000
+                                )
+                                .queryString(
+                                        "timestamp_end",
+                                        Instant.now().plus(Duration.standardHours(1).getMillis()).getMillis() / 1000
+                                )
+                                .getHttpRequest(),
+                        HttpStatus.NO_CONTENT_204
+                )
+        ).check();
+    }
+
+    @Test
+    public void testOk() {
+        // given
+        final long timestampStart = Instant.now().plus(Duration.standardMinutes(15).getMillis()).getMillis() / 1000;
+        final long timestampEnd = Instant.now().plus(Duration.standardMinutes(45).getMillis()).getMillis() / 1000;
+        final SimpleRoom room = new SimpleRoom("test_name", 12);
+        new ChainedFixtures(
+                new ClearAllRoomsFixture(api.rooms()),
+                new ClearAllBookingsFixture(api.bookings()),
+                new CreateRoomFixture(api.rooms(), room)
+        ).perform();
+        final WriteResult ids = new CreateBookingFixture(
+                api.bookings(),
+                new SimpleBooking(
+                        null,
+                        "test_user_id",
+                        room,
+                        new LogicalSlot(timestampStart, timestampEnd)
+                )
+        ).perform();
+
+        // when
+        final HttpRequest request = get("http://localhost:7000/rooms/test_name/bookings")
+                .queryString(
+                        "timestamp_start",
+                        Instant.now().getMillis() / 1000
+                )
+                .queryString(
+                        "timestamp_end",
+                        Instant.now().plus(Duration.standardHours(1).getMillis()).getMillis() / 1000
+                )
+                .getHttpRequest();
+
+        // then
+        new RequestWithBodyAssertion(
+                new RequestHasStatusCodeAssertion(request, HttpStatus.OK_200),
+                String.format(
+                        "[{\"id\":%d,\"user_id\":\"test_user_id\",\"room\":{\"name\":\"test_name\",\"capacity\":12},\"slot\":{\"timestamp_start\":%d,\"timestamp_end\":%d}}]",
+                        ids.iterator().next().getIdValue(),
+                        timestampStart,
+                        timestampEnd
+                )
+        ).check();
+    }
+
+    @Test
+    public void testRoomNotFound() {
+        new WithFixtureAssertion(
+                new ChainedFixtures(
+                        new ClearAllRoomsFixture(api.rooms()),
+                        new ClearAllBookingsFixture(api.bookings())
+                ),
+                new RequestWithBodyAssertion(
+                        new RequestHasStatusCodeAssertion(
+                                get("http://localhost:7000/rooms/test_name/bookings")
+                                        .queryString(
+                                                "timestamp_start",
+                                                Instant.now().getMillis() / 1000
+                                        )
+                                        .queryString(
+                                                "timestamp_end",
+                                                Instant.now().plus(Duration.standardHours(1).getMillis()).getMillis() / 1000
+                                        )
+                                        .getHttpRequest(),
+                                HttpStatus.NOT_FOUND_404
+                        ),
+                        "No room found for name 'test_name'"
+                )
+        ).check();
+    }
 }
