1a244b2db633e85c93c26d5c653539fe2334df59
Romain Rochegude <romain.rochegude@gmail.com>
2018-11-21 21:07:04 +0100

    get available rooms request

--- src/main/java/fr/guddy/roombookings/infra/requests/GetAvailableRoomsRequest.java
+++ src/main/java/fr/guddy/roombookings/infra/requests/GetAvailableRoomsRequest.java
@@ -1,4 +1,86 @@
 package fr.guddy.roombookings.infra.requests;
 
-public final class GetAvailableRoomsRequest {
+import fr.guddy.roombookings.domain.bookings.Bookings;
+import fr.guddy.roombookings.domain.room.JsonRoom;
+import fr.guddy.roombookings.domain.room.Room;
+import fr.guddy.roombookings.domain.rooms.Rooms;
+import fr.guddy.roombookings.domain.slot.LogicalSlot;
+import fr.guddy.roombookings.domain.slot.Slot;
+import fr.guddy.roombookings.infra.params.*;
+import io.javalin.Context;
+import org.eclipse.jetty.http.HttpStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class GetAvailableRoomsRequest implements Request {
+
+    private final Rooms rooms;
+    private final Bookings bookings;
+    private final int capacity;
+    private final Slot slot;
+
+    public GetAvailableRoomsRequest(final Rooms rooms, final Bookings bookings, final int capacity, final Slot slot) {
+        this.rooms = rooms;
+        this.bookings = bookings;
+        this.capacity = capacity;
+        this.slot = slot;
+    }
+
+    public GetAvailableRoomsRequest(final Rooms rooms,
+                                    final Bookings bookings,
+                                    final Parameter<Integer> capacity,
+                                    final Parameter<Long> timestampStart,
+                                    final Parameter<Long> timestampEnd) {
+        this(
+                rooms,
+                bookings,
+                capacity.value(),
+                new LogicalSlot(
+                        timestampStart.value(),
+                        timestampEnd.value()
+                )
+        );
+    }
+
+    public GetAvailableRoomsRequest(final Rooms rooms, final Bookings bookings, final Context context) {
+        this(
+                rooms,
+                bookings,
+                new IntegerParameter(
+                        new RequiredParameter<>(
+                                new QueryParameter("capacity", context)
+                        )
+                ),
+                new LongParameter(
+                        new RequiredParameter<>(
+                                new QueryParameter("timestamp_start", context)
+                        )
+                ),
+                new LongParameter(
+                        new RequiredParameter<>(
+                                new QueryParameter("timestamp_end", context)
+                        )
+                )
+        );
+    }
+
+    @Override
+    public void perform(final Context context) {
+        final List<Room> availableRooms = rooms.capableRooms(capacity)
+                .stream()
+                .filter(room ->
+                        bookings.bookingsForRoomInSlot(room, slot).isEmpty()
+                ).collect(Collectors.toList());
+        if (availableRooms.isEmpty()) {
+            context.status(HttpStatus.NO_CONTENT_204);
+        } else {
+            context.json(
+                    availableRooms.stream()
+                            .map(JsonRoom::new)
+                            .map(JsonRoom::map)
+                            .collect(Collectors.toList())
+            ).status(HttpStatus.OK_200);
+        }
+    }
 }
--- src/test/java/fr/guddy/roombookings/infra/requests/GetAvailableRoomsRequestTest.java
+++ src/test/java/fr/guddy/roombookings/infra/requests/GetAvailableRoomsRequestTest.java
@@ -1,5 +1,125 @@
-import static org.junit.Assert.*;
+package fr.guddy.roombookings.infra.requests;
 
-public class GetAvailableRoomsRequestTest {
+import fr.guddy.roombookings.domain.booking.SimpleBooking;
+import fr.guddy.roombookings.domain.fixtures.*;
+import fr.guddy.roombookings.domain.room.SimpleRoom;
+import fr.guddy.roombookings.domain.slot.LogicalSlot;
+import fr.guddy.roombookings.infra.ApiExternalResource;
+import fr.guddy.roombookings.infra.assertions.WithFixtureAssertion;
+import fr.guddy.roombookings.infra.assertions.requests.RequestHasStatusCodeAssertion;
+import fr.guddy.roombookings.infra.assertions.requests.RequestWithBodyAssertion;
+import org.eclipse.jetty.http.HttpStatus;
+import org.joda.time.Duration;
+import org.joda.time.Instant;
+import org.junit.ClassRule;
+import org.junit.Test;
 
+import static com.mashape.unirest.http.Unirest.get;
+
+public final class GetAvailableRoomsRequestTest {
+    @ClassRule
+    public static final ApiExternalResource api = new ApiExternalResource();
+
+    @Test
+    public void testNoContent() {
+        new WithFixtureAssertion(
+                new ChainedFixtures(
+                        new ClearAllRoomsFixture(api.rooms()),
+                        new ClearAllBookingsFixture(api.bookings())
+                ),
+                new RequestHasStatusCodeAssertion(
+                        get("http://localhost:7000/rooms")
+                                .queryString("capacity", 10)
+                                .queryString("timestamp_start", Instant.now().getMillis() / 1000)
+                                .queryString(
+                                        "timestamp_end",
+                                        Instant.now().plus(Duration.standardHours(1).getMillis()).getMillis() / 1000
+                                ).getHttpRequest(),
+                        HttpStatus.NO_CONTENT_204
+                )
+        ).check();
+    }
+
+    @Test
+    public void testMissingParameter() {
+        new WithFixtureAssertion(
+                new ChainedFixtures(
+                        new ClearAllRoomsFixture(api.rooms()),
+                        new ClearAllBookingsFixture(api.bookings())
+                ),
+                new RequestWithBodyAssertion(
+                        new RequestHasStatusCodeAssertion(
+                                get("http://localhost:7000/rooms")
+                                        .queryString("capacity", 10)
+                                        .queryString("timestamp_start", Instant.now().getMillis() / 1000)
+                                        .getHttpRequest(),
+                                HttpStatus.BAD_REQUEST_400
+                        ),
+                        "Parameter named 'timestamp_end' is missing"
+                )
+        ).check();
+    }
+
+    @Test
+    public void testOkWithAvailableRooms() {
+        new WithFixtureAssertion(
+                new ChainedFixtures(
+                        new ClearAllRoomsFixture(api.rooms()),
+                        new ClearAllBookingsFixture(api.bookings()),
+                        new CreateRoomFixture(
+                                api.rooms(),
+                                new SimpleRoom("test_name", 12)
+                        )
+                ),
+                new RequestWithBodyAssertion(
+                        new RequestHasStatusCodeAssertion(
+                                get("http://localhost:7000/rooms")
+                                        .queryString("capacity", 10)
+                                        .queryString("timestamp_start", Instant.now().getMillis() / 1000)
+                                        .queryString(
+                                                "timestamp_end",
+                                                Instant.now().plus(Duration.standardHours(1).getMillis()).getMillis() / 1000
+                                        ).getHttpRequest(),
+                                HttpStatus.OK_200
+                        ),
+                        "[{\"name\":\"test_name\",\"capacity\":12}]"
+                )
+        ).check();
+    }
+
+    @Test
+    public void testOkWithNoAvailableRooms() {
+        new WithFixtureAssertion(
+                new ChainedFixtures(
+                        new ClearAllRoomsFixture(api.rooms()),
+                        new ClearAllBookingsFixture(api.bookings()),
+                        new CreateRoomFixture(
+                                api.rooms(),
+                                new SimpleRoom("test_name", 12)
+                        ),
+                        new CreateBookingFixture(
+                                api.bookings(),
+                                new SimpleBooking(
+                                        null,
+                                        "test_user_id",
+                                        new SimpleRoom("test_name", 12),
+                                        new LogicalSlot(
+                                                Instant.now().plus(Duration.standardMinutes(15).getMillis()).getMillis() / 1000,
+                                                Instant.now().plus(Duration.standardMinutes(45).getMillis()).getMillis() / 1000
+                                        )
+                                )
+                        )
+                ),
+                new RequestHasStatusCodeAssertion(
+                        get("http://localhost:7000/rooms")
+                                .queryString("capacity", 10)
+                                .queryString("timestamp_start", Instant.now().getMillis() / 1000)
+                                .queryString(
+                                        "timestamp_end",
+                                        Instant.now().plus(Duration.standardHours(1).getMillis()).getMillis() / 1000
+                                ).getHttpRequest(),
+                        HttpStatus.NO_CONTENT_204
+                )
+        ).check();
+    }
 }
