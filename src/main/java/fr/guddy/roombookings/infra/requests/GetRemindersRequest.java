da237406d5bf1413c188c46ea7eff73f823fa0e3
Romain Rochegude <romain.rochegude@gmail.com>
2018-11-21 21:09:26 +0100

    get reminders request

--- src/main/java/fr/guddy/roombookings/infra/requests/GetRemindersRequest.java
+++ src/main/java/fr/guddy/roombookings/infra/requests/GetRemindersRequest.java
@@ -1,4 +1,56 @@
 package fr.guddy.roombookings.infra.requests;
 
-public final class GetRemindersRequest {
+import fr.guddy.roombookings.domain.booking.Booking;
+import fr.guddy.roombookings.domain.booking.JsonBooking;
+import fr.guddy.roombookings.domain.bookings.Bookings;
+import fr.guddy.roombookings.infra.params.Parameter;
+import fr.guddy.roombookings.infra.params.QueryParameter;
+import fr.guddy.roombookings.infra.params.RequiredParameter;
+import io.javalin.Context;
+import org.eclipse.jetty.http.HttpStatus;
+import org.joda.time.Instant;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class GetRemindersRequest implements Request {
+
+    private final Bookings bookings;
+    private final String userId;
+
+    public GetRemindersRequest(final Bookings bookings, final String userId) {
+        this.bookings = bookings;
+        this.userId = userId;
+    }
+
+    public GetRemindersRequest(final Bookings bookings, final Parameter<String> userId) {
+        this(bookings, userId.value());
+    }
+
+    public GetRemindersRequest(final Bookings bookings, final Context context) {
+        this(
+                bookings,
+                new RequiredParameter<>(
+                        new QueryParameter("user_id", context)
+                )
+        );
+    }
+
+    @Override
+    public void perform(final Context context) {
+        final List<Booking> reminders = bookings.bookingsForUserFromStartDate(
+                userId,
+                Instant.now().getMillis() / 1000
+        );
+        if (reminders.isEmpty()) {
+            context.status(HttpStatus.NO_CONTENT_204);
+        } else {
+            context.json(
+                    reminders.stream()
+                            .map(JsonBooking::new)
+                            .map(JsonBooking::map)
+                            .collect(Collectors.toList())
+            ).status(HttpStatus.OK_200);
+        }
+    }
 }
--- src/test/java/fr/guddy/roombookings/infra/requests/GetRemindersRequestTest.java
+++ src/test/java/fr/guddy/roombookings/infra/requests/GetRemindersRequestTest.java
@@ -1,5 +1,122 @@
-import static org.junit.Assert.*;
+package fr.guddy.roombookings.infra.requests;
 
-public class GetRemindersRequestTest {
+import com.mashape.unirest.request.HttpRequest;
+import fr.guddy.roombookings.domain.booking.SimpleBooking;
+import fr.guddy.roombookings.domain.fixtures.*;
+import fr.guddy.roombookings.domain.room.SimpleRoom;
+import fr.guddy.roombookings.domain.slot.LogicalSlot;
+import fr.guddy.roombookings.infra.ApiExternalResource;
+import fr.guddy.roombookings.infra.assertions.WithFixtureAssertion;
+import fr.guddy.roombookings.infra.assertions.requests.RequestHasStatusCodeAssertion;
+import fr.guddy.roombookings.infra.assertions.requests.RequestWithBodyAssertion;
+import org.dizitart.no2.WriteResult;
+import org.eclipse.jetty.http.HttpStatus;
+import org.joda.time.Duration;
+import org.joda.time.Instant;
+import org.junit.ClassRule;
+import org.junit.Test;
 
+import static com.mashape.unirest.http.Unirest.get;
+
+public final class GetRemindersRequestTest {
+    @ClassRule
+    public static final ApiExternalResource api = new ApiExternalResource();
+
+    @Test
+    public void testMissingParameter() {
+        new WithFixtureAssertion(
+                new ChainedFixtures(
+                        new ClearAllRoomsFixture(api.rooms()),
+                        new ClearAllBookingsFixture(api.bookings())
+                ),
+                new RequestWithBodyAssertion(
+                        new RequestHasStatusCodeAssertion(
+                                get("http://localhost:7000/bookings")
+                                        .getHttpRequest(),
+                                HttpStatus.BAD_REQUEST_400
+                        ),
+                        "Parameter named 'user_id' is missing"
+                )
+        ).check();
+    }
+
+    @Test
+    public void testNoContent() {
+        new WithFixtureAssertion(
+                new ChainedFixtures(
+                        new ClearAllRoomsFixture(api.rooms()),
+                        new ClearAllBookingsFixture(api.bookings())
+                ),
+                new RequestHasStatusCodeAssertion(
+                        get("http://localhost:7000/bookings")
+                                .queryString("user_id", "test@test.com")
+                                .getHttpRequest(),
+                        HttpStatus.NO_CONTENT_204
+                )
+        ).check();
+    }
+
+    @Test
+    public void testContent() {
+        // given
+        final long nowMinus45m = Instant.now().minus(Duration.standardMinutes(45).getMillis()).getMillis() / 1000;
+        final long nowMinus15m = Instant.now().minus(Duration.standardMinutes(15).getMillis()).getMillis() / 1000;
+        final long nowPlus15m = Instant.now().plus(Duration.standardMinutes(15).getMillis()).getMillis() / 1000;
+        final long nowPlus45m = Instant.now().plus(Duration.standardMinutes(45).getMillis()).getMillis() / 1000;
+        final SimpleRoom room = new SimpleRoom("test_name", 12);
+        new ChainedFixtures(
+                new ClearAllRoomsFixture(api.rooms()),
+                new ClearAllBookingsFixture(api.bookings()),
+                new CreateRoomFixture(api.rooms(), room),
+                new CreateBookingFixture(
+                        api.bookings(),
+                        new SimpleBooking(
+                                null,
+                                "test@test.com",
+                                room,
+                                new LogicalSlot(nowMinus45m, nowMinus15m)
+                        )
+                )
+        ).perform();
+        final WriteResult idMinus15ToPlus15m = new CreateBookingFixture(
+                api.bookings(),
+                new SimpleBooking(
+                        null,
+                        "test@test.com",
+                        room,
+                        new LogicalSlot(nowMinus15m, nowPlus15m)
+                )
+        ).perform();
+        final WriteResult id15To45m = new CreateBookingFixture(
+                api.bookings(),
+                new SimpleBooking(
+                        null,
+                        "test@test.com",
+                        room,
+                        new LogicalSlot(nowPlus15m, nowPlus45m)
+                )
+        ).perform();
+
+        // when
+        final HttpRequest request = get("http://localhost:7000/bookings")
+                .queryString("user_id", "test@test.com")
+                .getHttpRequest();
+
+        // then
+        new RequestWithBodyAssertion(
+                new RequestHasStatusCodeAssertion(request, HttpStatus.OK_200),
+                String.format(
+                        "[" +
+                                "{\"id\":%d,\"user_id\":\"test@test.com\",\"room\":{\"name\":\"test_name\",\"capacity\":12},\"slot\":{\"timestamp_start\":%d,\"timestamp_end\":%d}}," +
+                                "{\"id\":%d,\"user_id\":\"test@test.com\",\"room\":{\"name\":\"test_name\",\"capacity\":12},\"slot\":{\"timestamp_start\":%d,\"timestamp_end\":%d}}" +
+                                "]",
+                        idMinus15ToPlus15m.iterator().next().getIdValue(),
+                        nowMinus15m,
+                        nowPlus15m,
+                        id15To45m.iterator().next().getIdValue(),
+                        nowPlus15m,
+                        nowPlus45m
+                )
+        ).check();
+    }
 }
