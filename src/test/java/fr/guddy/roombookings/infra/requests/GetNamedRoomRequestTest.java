f647bda3ba7e2cf4fbf2d23f9eb08ff7ddb4bcfe
Romain Rochegude <romain.rochegude@gmail.com>
2018-11-21 21:09:49 +0100

    test for named rooms and all rooms request

--- src/test/java/fr/guddy/roombookings/infra/requests/GetNamedRoomRequestTest.java
+++ src/test/java/fr/guddy/roombookings/infra/requests/GetNamedRoomRequestTest.java
@@ -1,7 +1,56 @@
 package fr.guddy.roombookings.infra.requests;
 
-import static org.junit.Assert.*;
+import fr.guddy.roombookings.domain.fixtures.ChainedFixtures;
+import fr.guddy.roombookings.domain.fixtures.ClearAllRoomsFixture;
+import fr.guddy.roombookings.domain.fixtures.CreateRoomFixture;
+import fr.guddy.roombookings.domain.room.SimpleRoom;
+import fr.guddy.roombookings.infra.ApiExternalResource;
+import fr.guddy.roombookings.infra.assertions.WithFixtureAssertion;
+import fr.guddy.roombookings.infra.assertions.requests.RequestHasStatusCodeAssertion;
+import fr.guddy.roombookings.infra.assertions.requests.RequestWithBodyAssertion;
+import org.eclipse.jetty.http.HttpStatus;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import static com.mashape.unirest.http.Unirest.get;
 
 public final class GetNamedRoomRequestTest {
+    @ClassRule
+    public static final ApiExternalResource api = new ApiExternalResource();
 
+    @Test
+    public void testNotFound() {
+        new WithFixtureAssertion(
+                new ChainedFixtures(
+                        new ClearAllRoomsFixture(api.rooms())
+                ),
+                new RequestWithBodyAssertion(
+                        new RequestHasStatusCodeAssertion(
+                                get("http://localhost:7000/rooms/test_name"),
+                                HttpStatus.NOT_FOUND_404
+                        ),
+                        "No room found for name 'test_name'"
+                )
+        ).check();
+    }
+
+    @Test
+    public void testOK() {
+        new WithFixtureAssertion(
+                new ChainedFixtures(
+                        new ClearAllRoomsFixture(api.rooms()),
+                        new CreateRoomFixture(
+                                api.rooms(),
+                                new SimpleRoom("test_name", 12)
+                        )
+                ),
+                new RequestWithBodyAssertion(
+                        new RequestHasStatusCodeAssertion(
+                                get("http://localhost:7000/rooms/test_name"),
+                                HttpStatus.OK_200
+                        ),
+                        "{\"name\":\"test_name\",\"capacity\":12}"
+                )
+        ).check();
+    }
 }
--- src/test/java/fr/guddy/roombookings/infra/requests/GetRoomsRequestTest.java
+++ src/test/java/fr/guddy/roombookings/infra/requests/GetRoomsRequestTest.java
@@ -1,54 +1,51 @@
 package fr.guddy.roombookings.infra.requests;
 
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.exceptions.UnirestException;
-import com.mashape.unirest.request.GetRequest;
+import fr.guddy.roombookings.domain.fixtures.ChainedFixtures;
+import fr.guddy.roombookings.domain.fixtures.ClearAllRoomsFixture;
+import fr.guddy.roombookings.domain.fixtures.CreateRoomFixture;
 import fr.guddy.roombookings.domain.room.SimpleRoom;
-import fr.guddy.roombookings.domain.rooms.NitriteRooms;
-import fr.guddy.roombookings.domain.rooms.Rooms;
 import fr.guddy.roombookings.infra.ApiExternalResource;
-import org.dizitart.no2.Nitrite;
+import fr.guddy.roombookings.infra.assertions.WithFixtureAssertion;
+import fr.guddy.roombookings.infra.assertions.requests.RequestHasStatusCodeAssertion;
+import fr.guddy.roombookings.infra.assertions.requests.RequestWithBodyAssertion;
 import org.eclipse.jetty.http.HttpStatus;
-import org.junit.Rule;
+import org.junit.ClassRule;
 import org.junit.Test;
 
 import static com.mashape.unirest.http.Unirest.get;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public final class GetRoomsRequestTest {
-    private final Nitrite database = Nitrite.builder().openOrCreate();
-    private final Rooms rooms = new NitriteRooms(database);
-    @Rule
-    public final ApiExternalResource apiExternalResource = new ApiExternalResource(
-            7000,
-            rooms
-    );
+    @ClassRule
+    public static final ApiExternalResource api = new ApiExternalResource();
 
     @Test
-    public void testNoContent() throws UnirestException {
-        // given
-        final GetRequest request = get("http://localhost:7000/rooms");
-
-        // when
-        final HttpResponse<String> response = request.asString();
-
-        // then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.NO_CONTENT_204);
+    public void testNoContent() {
+        new WithFixtureAssertion(
+                new ClearAllRoomsFixture(api.rooms()),
+                new RequestHasStatusCodeAssertion(
+                        get("http://localhost:7000/rooms"),
+                        HttpStatus.NO_CONTENT_204
+                )
+        ).check();
     }
 
     @Test
-    public void testOK() throws UnirestException {
-        // given
-        rooms.create(new SimpleRoom("test_name", 12));
-        final GetRequest request = get("http://localhost:7000/rooms");
-
-        // when
-        final HttpResponse<String> response = request.asString();
-
-        // then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK_200);
-        assertThat(
-                response.getBody()
-        ).isEqualToIgnoringCase("[{\"name\":\"test_name\",\"capacity\":12}]");
+    public void testOK() {
+        new WithFixtureAssertion(
+                new ChainedFixtures(
+                        new ClearAllRoomsFixture(api.rooms()),
+                        new CreateRoomFixture(
+                                api.rooms(),
+                                new SimpleRoom("test_name", 12)
+                        )
+                ),
+                new RequestWithBodyAssertion(
+                        new RequestHasStatusCodeAssertion(
+                                get("http://localhost:7000/rooms"),
+                                HttpStatus.OK_200
+                        ),
+                        "[{\"name\":\"test_name\",\"capacity\":12}]"
+                )
+        ).check();
     }
 }
