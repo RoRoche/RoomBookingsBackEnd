bd2f7e0b0dee7f31028929d56f4abb33e86aa354
Romain Rochegude <romain.rochegude@gmail.com>
2018-11-26 21:56:19 +0100

    migrate Rooms classes to kotlin

--- src/main/java/fr/guddy/roombookings/domain/room/JsonRoom.kt
+++ src/main/java/fr/guddy/roombookings/domain/room/JsonRoom.kt
@@ -1,34 +1,24 @@
 package fr.guddy.roombookings.domain.room
 
+import java.io.StringReader
+import java.util.*
 import javax.json.Json
 import javax.json.JsonObject
-import java.io.StringReader
-import java.util.LinkedHashMap
 
-class JsonRoom(private val delegate: Room) : Room {
+class JsonRoom(private val delegate: Room) : Room by delegate {
 
     constructor(body: String) : this(
             Json.createReader(
                     StringReader(body)
             ).readObject()
-    ) {
-    }
+    )
 
     constructor(jsonObject: JsonObject) : this(
             SimpleRoom(
                     jsonObject.getString(JSON_KEY_NAME),
                     jsonObject.getInt(JSON_KEY_CAPACITY)
             )
-    ) {
-    }
-
-    override fun name(): String {
-        return delegate.name()
-    }
-
-    override fun capacity(): Int {
-        return delegate.capacity()
-    }
+    )
 
     override fun map(): Map<String, Any> {
         val map = LinkedHashMap<String, Any>()
@@ -38,7 +28,7 @@ class JsonRoom(private val delegate: Room) : Room {
     }
 
     companion object {
-        private val JSON_KEY_NAME = "name"
-        private val JSON_KEY_CAPACITY = "capacity"
+        private const val JSON_KEY_NAME = "name"
+        private const val JSON_KEY_CAPACITY = "capacity"
     }
 }
