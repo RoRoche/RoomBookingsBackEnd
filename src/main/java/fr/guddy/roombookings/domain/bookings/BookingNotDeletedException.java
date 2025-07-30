4eb98f0a4b92e5f1800ae3b86bb6352aacbaa8be
Romain Rochegude <romain.rochegude@gmail.com>
2025-07-30 22:43:42 +0100

    :art: exception message in ctor

--- src/main/java/fr/guddy/roombookings/domain/bookings/BookingNotDeletedException.java
+++ src/main/java/fr/guddy/roombookings/domain/bookings/BookingNotDeletedException.java
@@ -2,8 +2,6 @@ package fr.guddy.roombookings.domain.bookings;
 
 public final class BookingNotDeletedException extends RuntimeException {
     public BookingNotDeletedException(final long id) {
-        super(
-                String.format("Booking with id %d could not be deleted", id)
-        );
+        super(String.format("Booking with id %d could not be deleted", id));
     }
 }
--- src/main/java/fr/guddy/roombookings/domain/bookings/BookingNotFoundException.java
+++ src/main/java/fr/guddy/roombookings/domain/bookings/BookingNotFoundException.java
@@ -2,8 +2,6 @@ package fr.guddy.roombookings.domain.bookings;
 
 public final class BookingNotFoundException extends RuntimeException {
     public BookingNotFoundException(final long id) {
-        super(
-                String.format("No booking with id %d were found", id)
-        );
+        super(String.format("No booking with id %d were found", id));
     }
 }
