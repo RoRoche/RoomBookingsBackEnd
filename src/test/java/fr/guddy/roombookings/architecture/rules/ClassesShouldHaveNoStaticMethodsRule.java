708c0451fb304bae2ea72232618d3b0a3e1c70b9
Romain Rochegude <romain.rochegude@gmail.com>
2025-11-08 16:31:40 +0100

    :truck: rename condition

--- src/test/java/fr/guddy/roombookings/architecture/rules/ClassesShouldHaveNoStaticMethodsRule.java
+++ src/test/java/fr/guddy/roombookings/architecture/rules/ClassesShouldHaveNoStaticMethodsRule.java
@@ -3,8 +3,8 @@ package fr.guddy.roombookings.architecture.rules;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import fr.guddy.roombookings.architecture.rules.conditions.HaveNoStaticMethods;
 
-public final class NoStaticMethodsRule extends ArchRuleEnvelope {
-    public NoStaticMethodsRule() {
+public final class ClassesShouldHaveNoStaticMethodsRule extends ArchRuleEnvelope {
+    public ClassesShouldHaveNoStaticMethodsRule() {
         super(
                 ArchRuleDefinition.classes().should(new HaveNoStaticMethods())
         );
