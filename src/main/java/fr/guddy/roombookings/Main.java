a27debb2632772152067c72f403f8e8ebe9f523f
Romain Rochegude <romain.rochegude@gmail.com>
2025-11-12 18:12:35 +0100

    :sparkles: explicitely stop the application on shutdown

--- src/main/java/fr/guddy/roombookings/Main.java
+++ src/main/java/fr/guddy/roombookings/Main.java
@@ -1,9 +1,12 @@
 package fr.guddy.roombookings;
 
 import fr.guddy.roombookings.infra.Api;
+import fr.guddy.roombookings.infra.Application;
 
 public final class Main {
     public static void main(final String[] args) {
-        new Api().start();
+        final Application api = new Api();
+        Runtime.getRuntime().addShutdownHook(new Thread(api::stop));
+        api.start();
     }
 }
